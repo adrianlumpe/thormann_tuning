7e61b1c8eea8be6ec7221f868e3250881f69b5f6
EpicProgger <lumpe@4ft.de>
2018-05-11 02:12:09 +0200

    adds more webpack stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-//const CustomCSS = new ExtractTextPlugin('./assets/css/custom.css');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlPluginRemove = require('html-webpack-plugin-remove');
+
+const VendorCSS = new ExtractTextPlugin('./assets/css/vendor.[contenthash:8].css');
+const CustomCSS = new ExtractTextPlugin('./assets/css/main.[contenthash:8].css');
 
 module.exports = {
   entry: {
@@ -16,16 +20,53 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
+        test: /vendor.sass$/,
+        use: VendorCSS.extract({
           fallback: 'style-loader',
-          use: 'css-loader'
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true,
+                url: false // don't parse image URLs in CSS files
+              }
+            },
+            'sass-loader'
+          ]
+        })
+      },
+      {
+        test: /main.sass$/,
+        use: CustomCSS.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true,
+                url: false // don't parse image URLs in CSS files
+              }
+            },
+            'sass-loader'
+          ]
         })
       }
     ]
   },
   plugins: [
     new CleanWebpackPlugin(['user/themes/alice/assets/css/*.*', 'user/themes/alice/assets/js/*.*']),
-    new ExtractTextPlugin('./assets/css/vendor.[contenthash:8].css')
+    VendorCSS,
+    CustomCSS,
+    new HtmlWebpackPlugin({
+      template: './user/themes/alice/source/twig/head.html.twig',
+      filename: './templates/partials/head.html.twig',
+      chunks: ['head']
+    }),
+    new HtmlWebpackPlugin({
+      template: './user/themes/alice/source/twig/footer.html.twig',
+      filename: './templates/partials/footer.html.twig',
+      chunks: ['footer']
+    }),
+    new HtmlPluginRemove(/<script.*?src=".*?\/head\..*?\.js".*?<\/script>/)
   ]
 };
