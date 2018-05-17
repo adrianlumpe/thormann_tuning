e7f1f7366bb6bcf85073c8d1403c7480b9770037
Flo <me@flomotionstudios.com>
2018-05-17 23:57:04 +0200

    Sidenav for Mobile and Navbar for Desktop v2 --- Sidenav öffnet und schließt mit selben Button BB transformiert sich

--- user/themes/alice/source/js/footer.js
+++ user/themes/alice/source/js/footer.js
@@ -8,4 +8,16 @@ $(document).ready(function(){
     items:1,
     loop:true,
   });
+
+  $("#menubutton").click(function(){
+    var sideNav = M.Sidenav.getInstance($(".sidenav"));
+    if(sideNav.isOpen) {
+      sideNav.close();
+      $(this).css("transform", "rotate(0deg)");
+    }
+    else {
+      sideNav.open();
+        $(this).css("transform", "rotate(-90deg)");
+    }
+  });
 });
